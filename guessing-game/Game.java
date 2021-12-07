c89b41dd16b468e947a4877476a4e9f98127dbfa
Christian Colson <christiancolson91@gmail.com>
2021-12-07 14:42:10 -0600

    first commit-greeting player

---
+++ guessing-game/Game.java
@@ -0,0 +1,23 @@
+/*
+A number-guessing game.
+
+greet player
+get player name
+choose random number b/w 1 & 100
+repeat forever:
+  get guess
+  if guess is incorrect:
+    give hint
+    increase number of guesses
+  else:
+    congratulate player
+*/
+
+public class Game {
+  public static void main(String[] args){
+    // System.out.println("Hello Christian!");
+    System.out.println("Step right up new contestant! What's your name?");
+
+  }
+
+}

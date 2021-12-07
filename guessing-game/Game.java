b2ebfe73ccc8c6bec8e1c48f2276366e4621c0f7
Christian Colson <christiancolson91@gmail.com>
2021-12-07 17:12:20 -0600

    guessing game completed

--- guessing-game/Game.java
+++ guessing-game/Game.java
@@ -12,12 +12,43 @@ repeat forever:
   else:
     congratulate player
 */
+import java.util.Scanner;
+import java.util.Random;
 
 public class Game {
   public static void main(String[] args){
     // System.out.println("Hello Christian!");
+    Scanner input = new Scanner(System.in);
+    // Creates an instance of the Random object
+    Random rand = new Random();
+    // Call method nextInt and pass in upper range bound
+    int number = rand.nextInt(100) + 1;
+    System.out.println("Random number is " + number);
+
     System.out.println("Step right up new contestant! What's your name?");
 
-  }
+    String name = input.nextLine();
+    System.out.println(name + ", I'm thinking of a number between 1 and 100.");
+    System.out.println("Try to guess my number.");
+    
+    int tryCount = 0;
 
+    while(true) {
+      System.out.print("Your guess?");
+      int guess = input.nextInt();
+      tryCount++;
+
+      if(guess == number){
+        System.out.println("Correct! You Win!");
+        System.out.println("It took you " + tryCount + " tries!");
+        break;
+      }
+      else if(number > guess){
+        System.out.println("Nope! The number is higher. Guess again.");
+      }
+      else {
+        System.out.println("Nope! The number is lower. Guess again.");
+      }
+    }
+  }
 }

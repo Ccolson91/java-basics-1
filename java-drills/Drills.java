c96be90f3863869f1975042fbf4bde7d5852734f
Christian Colson <christiancolson91@gmail.com>
2021-12-08 23:10:06 -0600

    mathematical operators complete

--- java-drills/Drills.java
+++ java-drills/Drills.java
@@ -1,11 +1,21 @@
 public class Drills {
   public static void main(String[] args) {
+    // Variables & Data Types
     String fName = "Luke";
     String lName = "Skywalker";
     String bYear = "19 BBY";
     String home = "Tatooine";
     float heightM = 1.72f;
     float weightKg = 73f;
-
+    // Mathematical Operators
+    float purse = 150.00f;
+    float droid = -24.3f;
+    float boots = -45f;
+    purse += droid + boots;
+    purse = purse * 2;
+    purse -= purse / 10;
+    System.out.println(purse);
+    // Conditional Statements
+    
   }
 }

e95d8a2f337fda7835c9d1632d9b50600518cd52
Christian Colson <christiancolson91@gmail.com>
2021-12-08 22:40:15 -0600

    variable names complete

---
+++ java-drills/Drills.java
@@ -0,0 +1,11 @@
+public class Drills {
+  public static void main(String[] args) {
+    String fName = "Luke";
+    String lName = "Skywalker";
+    String bYear = "19 BBY";
+    String home = "Tatooine";
+    float heightM = 1.72f;
+    float weightKg = 73f;
+
+  }
+}
